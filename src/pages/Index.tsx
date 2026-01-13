a972b1ad025c66f0c5d30a86950d5c0c5ac7ad5e
shoaib7262 <ranashobi302@gmail.com>
2026-01-14 01:01:40 +0500

    adaptableSection activities

--- src/pages/Index.tsx
+++ src/pages/Index.tsx
@@ -6,12 +6,14 @@ import WhyChooseUsSection from "@/components/home/WhyChooseUsSection";
 import TestimonialsSection from "@/components/home/TestimonialsSection";
 import TeamSection from "@/components/home/TeamSection";
 import CTASection from "@/components/home/CTASection";
+import AdaptableSection from "@/components/home/AdaptableSection";
 
 const Index = () => {
   return (
     <Layout>
       <HeroSection />
       <ServicesSection />
+      <AdaptableSection/>
       <ProjectsSection />
       <WhyChooseUsSection />
       <TestimonialsSection />
